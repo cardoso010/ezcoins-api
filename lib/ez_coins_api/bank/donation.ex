69db90fb6d2f23647bf0c2b62eceeb8cbad70ddc
maykon@deepin <maykonmichelpalma@gmail.com>
2019-10-24 18:24:51 -0300

    Added donation model

---
+++ lib/ez_coins_api/bank/donation.ex
@@ -0,0 +1,22 @@
+defmodule EzCoinsApi.Bank.Donation do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "donations" do
+    field :quantity, :integer
+    field :reason, :string
+    field :sender, :id
+    field :receiver, :id
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(donation, attrs) do
+    donation
+    |> cast(attrs, [:quantity, :reason, :sender, :receiver])
+    |> validate_required([:quantity, :reason, :sender, :receiver])
+    |> validate_number(:quantity, grater_than: 0)
+    |> validate_length(:reason, min: 5)
+  end
+end
